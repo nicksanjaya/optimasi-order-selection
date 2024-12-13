b86a7cf09ec7b67f3aef6483f3927ea589147686
nicksanjaya <nicky.sanjaya@gmail.com>
2024-12-13 13:41:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -74,10 +74,10 @@ def solve_optimization(df,order,capacity):
         value =  part_value * df.Margin[i]
         margin.append(value)
         if pyo.value(pn[i]) > 0:
-            st.write(f'<center><b><h3>Part Number: {df.PN[i]} = {pyo.value(pn[i]),.0f}</b></h3>', unsafe_allow_html=True)
+            st.write(f'<center><b><h3>Part Number: {df.PN[i]} = {pyo.value(pn[i]):,.0f}</b></h3>', unsafe_allow_html=True)
             
     total_margin = sum(margin)
-    st.write('<center><b><h3>Total Margin: =', total_margin, '</b></h3>', unsafe_allow_html=True)
+    st.write(f'<center><b><h3>Total Margin: = {total_margin:,.0f} </b></h3>', unsafe_allow_html=True)
 
 # Upload Excel file
 uploaded_file = st.file_uploader("Upload Excel Master Data", type=["xlsx"])
