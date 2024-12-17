eb7ab7b0ab877db9acb5a678cd0e71af44f7fe35
nicksanjaya <nicky.sanjaya@gmail.com>
2024-12-17 09:56:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ def solve_optimization(df,order,capacity):
         value =  part_value * df.Margin[i]
         margin.append(value)
         if pyo.value(pn[i]) > 0:
-            st.write(f'<center><b><h3>Part Number: {df.PN[i]} = {pyo.value(pn[i]):,.0f}</b></h3>', unsafe_allow_html=True)
+            st.write(f'<center><b><h3>Part Number: {df.PN[i]} = {pyo.value(pn[i]):,.0f} pcs</b></h3>', unsafe_allow_html=True)
             
     total_margin = sum(margin)
     st.write(f'<center><b><h3>Total Margin: {total_margin:,.0f} </b></h3>', unsafe_allow_html=True)
